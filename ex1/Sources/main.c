167b6ce4ce701e3e8afa1e6a95b7540fc6d9654f
DarraghMcMullen <dmcm9019@uni.sydney.edu.au>
2021-04-20 15:59:02 +1000

    commented and refactored code

--- ex1/Sources/main.c
+++ ex1/Sources/main.c
@@ -3,8 +3,6 @@
 
 #include "timers.h"
 
-
-
 void main(void) {
 
   unsigned int time1, time2;
@@ -14,57 +12,46 @@ void main(void) {
   float timePerTick = 41.6; // time per tick in ns 41.6ns for prescaler = 1
   long oFval = 65536; // time elapsed per time overflow
   
-  //TSCR1 = 0x90; // enable timer counter, enable fast flag clear
-  //TSCR2 = 0x80; // enable TCNT overflow interrupt, set prescaler to 1
-  
-  Init_TCNT();
+  Init_TCNT(); // initialise timer
   
   a = 5;
   b = 10;
   resetOF();
-  time1 = TCNT; // save initial timer
+  time1 = TCNT; // save initial time
   c = b + a;
   time2 = TCNT;
   times[0] = (time2 - time1) + (overFlows * oFval); //calculate period
   
-  a = 5;
-  b = 10;
   resetOF();
-  time1 = TCNT; // save initial timer
+  time1 = TCNT; // save initial time
   c = b * a;
   time2 = TCNT;
   times[1] = (time2 - time1) + (overFlows * oFval); //calculate period
   
-  a = 5;
-  b = 10;
   resetOF();
-  time1 = TCNT; // save initial timer
+  time1 = TCNT; // save initial time
   c = b / a;
   time2 = TCNT;
   times[2] = (time2 - time1) + (overFlows * oFval); //calculate period
   
-  a = 5;
-  b = 10;
   resetOF();
-  time1 = TCNT; // save initial timer
+  time1 = TCNT; // save initial time
   c = sqrt(b);
   time2 = TCNT;
   times[3] = (time2 - time1) + (overFlows * oFval); //calculate period
   
-  a = 5;
-  b = 10;
   resetOF();
-  time1 = TCNT; // save initial timer
+  time1 = TCNT; // save initial time
   c = sin(b);
   time2 = TCNT;
   times[4] = (time2 - time1) + (overFlows * oFval); //calculate period
   
-  asm ("swi");
-  
+  // calculate time elapsed for each function
   for(a = 0; a < 5; a++){
     ns[a] = times[a] * timePerTick; 
   }
 
+  asm ("swi");
 
 	EnableInterrupts;
 
