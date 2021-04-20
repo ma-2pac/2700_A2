254d36f6a2f245949c1a5ca47db4f1c8717c6ee3
fergus Charles <ferguscharles7@gmail.com>
2021-04-20 16:25:23 +1000

    adding work from today - port h

---
+++ ex3.1/Sources/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <hidef.h>      /* common defines and macros */
+#include "derivative.h"      /* derivative-specific definitions */
+
+#include "pwm.h"
+#include "input.h"
+
+
+void main(void) {
+  int Hi_count;
+  //int analog_input;
+
+  //call the timer function
+  
+  DDRH = 0x00;
+  while(1){
+    Hi_count = Duty_Hi_Calculator();
+  }
+
+  
+  Init_TC5();
+
+	EnableInterrupts;
+
+  //enable led 
+  DDRB = 0xFF;
+  DDRJ = 0xF;
+  PTJ = 0x00; 
+  
+  
+  //DDRT = 0x20;
+  
+  //read_analog();
+
+  
+
+  for(;;) {
+    _FEED_COP(); /* feeds the dog */
+  } /* loop forever */
+  /* please make sure that you never leave main */
+}
